13f4533b150375beba8100ed265ddfaf2309b77c
svladimirtsk-crypto <s.vladimir.tsk@gmail.com>
2026-04-13 20:38:57 +0300

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ReactNode } from "react";
+import dynamic from "next/dynamic";
+
+import { PriceCalculatorProvider } from "@/components/home/price-calculator-context";
+import { CalculatorModalProvider } from "@/components/calculator-modal/calculator-modal-context";
+
+const CalculatorModal = dynamic(
+  () =>
+    import("@/components/calculator-modal/calculator-modal").then(
+      (m) => m.CalculatorModal
+    ),
+  { ssr: false }
+);
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <PriceCalculatorProvider>
+      <CalculatorModalProvider>
+        {children}
+        <CalculatorModal />
+      </CalculatorModalProvider>
+    </PriceCalculatorProvider>
+  );
+}
