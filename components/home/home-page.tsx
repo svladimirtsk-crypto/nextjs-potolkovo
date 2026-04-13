6e182886df6dfba1be186686e22a727078166411
svladimirtsk-crypto <s.vladimir.tsk@gmail.com>
2026-04-13 20:48:33 +0300

    Update home-page.tsx

--- components/home/home-page.tsx
+++ components/home/home-page.tsx
@@ -1,33 +1,27 @@
-import { HomeAction } from "./home-action";
-import { HomeFooter } from "./home-footer";
 import { HomeHeader } from "./home-header";
 import { HomeHero } from "./home-hero";
-import { HomePrice } from "./home-price";
-import { HomePromise } from "./home-promise";
 import { HomeProof } from "./home-proof";
+import { HomePrice } from "./home-price";
 import { HomeTrust } from "./home-trust";
+import { HomePromise } from "./home-promise";
+import { HomeAction } from "./home-action";
+import { HomeFooter } from "./home-footer";
 import { MobileStickyCta } from "./mobile-sticky-cta";
-import { PriceCalculatorProvider } from "./price-calculator-context";
 
 export function HomePage() {
   return (
     <>
       <HomeHeader />
-
       <div className="pb-24 lg:pb-0">
         <main>
           <HomeHero />
-
-          <PriceCalculatorProvider>
-            <HomeProof />
-            <HomePrice />
-            <HomeTrust />
-            <HomePromise />
-            <HomeAction />
-            <MobileStickyCta />
-          </PriceCalculatorProvider>
+          <HomeProof />
+          <HomePrice />
+          <HomeTrust />
+          <HomePromise />
+          <HomeAction />
         </main>
-
+        <MobileStickyCta />
         <HomeFooter />
       </div>
     </>
