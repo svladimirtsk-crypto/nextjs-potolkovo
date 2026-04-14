d86d16e3b3c7bfe3cb7cb20d90a3953bc91f8e3b
svladimirtsk-crypto <s.vladimir.tsk@gmail.com>
2026-04-14 18:52:32 +0300

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import { type ReactNode } from "react";
 import dynamic from "next/dynamic";
 
 import { PriceCalculatorProvider } from "@/components/home/price-calculator-context";
