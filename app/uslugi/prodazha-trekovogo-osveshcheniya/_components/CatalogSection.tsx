313f3900b62d1453e645da1a946cf6eaabc15d89
svladimirtsk-crypto <s.vladimir.tsk@gmail.com>
2026-04-19 21:43:12 +0300

    Change data fetching function in CatalogSection

--- app/uslugi/prodazha-trekovogo-osveshcheniya/_components/CatalogSection.tsx
+++ app/uslugi/prodazha-trekovogo-osveshcheniya/_components/CatalogSection.tsx
@@ -1,8 +1,8 @@
 // app/uslugi/prodazha-trekovogo-osveshcheniya/_components/CatalogSection.tsx
-import { getFeed2Catalog } from "@/lib/eks-feed2-catalog";
+import { getCatalogData } from "@/lib/eks-feed2-catalog";
 import { CatalogSectionClient } from "./CatalogSectionClient";
 
 export async function CatalogSection() {
-  const data = await getFeed2Catalog();
+  const data = await getCatalogData();
   return <CatalogSectionClient data={data} />;
 }
