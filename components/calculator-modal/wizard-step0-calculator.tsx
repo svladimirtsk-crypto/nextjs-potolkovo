c92df0384a9eb0d3db2e0d93e80d47e7e73b2f28
svladimirtsk-crypto <s.vladimir.tsk@gmail.com>
2026-04-22 17:59:29 +0300

    Update wizard-step0-calculator.tsx

--- components/calculator-modal/wizard-step0-calculator.tsx
+++ components/calculator-modal/wizard-step0-calculator.tsx
@@ -2,11 +2,22 @@
 
 import type { ServiceCalculatorPreset } from "@/content/services";
 import { PriceCalculatorClient } from "@/components/home/price-calculator-client";
+import { useCalculatorModal } from "./calculator-modal-context";
 
 type WizardStep0CalculatorProps = {
   preset?: ServiceCalculatorPreset;
 };
 
 export function WizardStep0Calculator({ preset }: WizardStep0CalculatorProps) {
-  return <PriceCalculatorClient preset={preset} compactSections />;
+  const { markStep0SessionInteracted } = useCalculatorModal();
+
+  return (
+    <div
+      onPointerDown={markStep0SessionInteracted}
+      onKeyDown={markStep0SessionInteracted}
+      onChangeCapture={markStep0SessionInteracted}
+    >
+      <PriceCalculatorClient preset={preset} compactSections />
+    </div>
+  );
 }
