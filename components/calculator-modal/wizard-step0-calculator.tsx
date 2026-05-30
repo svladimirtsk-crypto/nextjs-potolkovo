717428b770e8478a3a7045f07230770419d91a57
svladimirtsk-crypto <s.vladimir.tsk@gmail.com>
2026-05-30 19:43:28 +0300

    Use resolved preset in PriceCalculatorClient

--- components/calculator-modal/wizard-step0-calculator.tsx
+++ components/calculator-modal/wizard-step0-calculator.tsx
@@ -11,13 +11,24 @@ type WizardStep0CalculatorProps = {
 export function WizardStep0Calculator({ preset }: WizardStep0CalculatorProps) {
   const { markStep0SessionInteracted } = useCalculatorModal();
 
+  const resolvedPreset: ServiceCalculatorPreset = {
+    ceilingType: String(preset?.ceilingType ?? "standard") as ServiceCalculatorPreset["ceilingType"],
+    areaDefault: Number(preset?.areaDefault ?? 10),
+    corniceType: preset?.corniceType,
+    trackType: preset?.trackType,
+    lightsEnabled: preset?.lightsEnabled,
+    lightsCount: preset?.lightsCount,
+    introNote: preset?.introNote,
+    lightingDefault: preset?.lightingDefault,
+  };
+
   return (
     <div
       onPointerDown={markStep0SessionInteracted}
       onKeyDown={markStep0SessionInteracted}
       onChangeCapture={markStep0SessionInteracted}
     >
-      <PriceCalculatorClient preset={preset} compactSections />
+      <PriceCalculatorClient preset={resolvedPreset} compactSections />
     </div>
   );
 }
