b1ef1e7270f6282e4e42a113e1162e11556a3746
svladimirtsk-crypto <s.vladimir.tsk@gmail.com>
2026-05-31 13:40:26 +0300

    Update CatalogSection.tsx

--- app/uslugi/prodazha-trekovogo-osveshcheniya/_components/CatalogSection.tsx
+++ app/uslugi/prodazha-trekovogo-osveshcheniya/_components/CatalogSection.tsx
@@ -1,20 +1,8 @@
 import { getCatalogData } from "@/lib/eks-feed2-catalog";
+
 import { CatalogSectionClient } from "./CatalogSectionClient";
-import { isRemovedColibriVendorCode } from "@/lib/catalog-ui-config";
 
 export async function CatalogSection() {
   const data = await getCatalogData();
-
-  const filteredProducts = (data.products ?? []).filter((product) => {
-    return !isRemovedColibriVendorCode(product.vendorCode);
-  });
-
-  return (
-    <CatalogSectionClient
-      data={{
-        ...data,
-        products: filteredProducts,
-      }}
-    />
-  );
+  return <CatalogSectionClient data={data} />;
 }
